c583f74ac22a6c22908c2eab85b6407eb50be054
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2025-06-18 12:30:23 +0530

    Update route.ts

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch("http://13.234.110.97:8000/users", {
+    const response = await fetch("http://13.204.45.108:8000/users", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
