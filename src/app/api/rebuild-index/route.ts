2ca420e4cdfe793643f10cb89959deee4f6b9d31
Subham Mishra <63531369+msubham193@users.noreply.github.com>
2025-06-18 12:33:59 +0530

    Update route.ts

--- src/app/api/rebuild-index/route.ts
+++ src/app/api/rebuild-index/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function POST() {
   try {
-    const response = await fetch("http://13.234.110.97:8000/rebuild-index", {
+    const response = await fetch("http://13.204.45.108:8000/rebuild-index", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
